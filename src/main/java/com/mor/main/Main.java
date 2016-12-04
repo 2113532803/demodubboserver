b0e44706cb2ddcb3bcf797fb3c174db4f25ec647
2113532803 <linyongboole@sina.cn>
2016-12-04 22:08:33 +0800

    在master做了操作

--- src/main/java/com/mor/main/Main.java
+++ src/main/java/com/mor/main/Main.java
@@ -24,6 +24,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "applicationProvider.xml" });
 		context.start();
+		System.out.println("在master分支下做操作");
 		System.out.println("按任意键退出");
 		System.in.read();
 	}
