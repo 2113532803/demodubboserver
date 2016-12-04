76f608fc28bdaca46c05c1f5fe508aaa4d01ca93
2113532803 <linyongboole@sina.cn>
2016-12-04 22:04:00 +0800

    main中在lingy分支添加内容

--- src/main/java/com/mor/main/Main.java
+++ src/main/java/com/mor/main/Main.java
@@ -24,6 +24,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "applicationProvider.xml" });
 		context.start();
+		System.out.println("lingy分支下添加的内容");
 		System.out.println("按任意键退出");
 		System.in.read();
 	}
